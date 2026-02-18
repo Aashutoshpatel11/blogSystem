51a647d0481f9b187e10201b36b688241ce106ca
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2026-02-18 15:41:57 +0530

    add css for blog card

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -22,20 +22,29 @@ function App() {
 
   return (
     <>
-      <div className="w-screen min-h-screen p-10 flex flex-col gap-10 items-center" >
+      <div className="w-screen min-h-screen p-10 flex flex-col gap-10 items-center">
         <h1 className="text-3xl font-bold underline">
           Blog System
         </h1>
-        <div className="flex gap-5 overflow-hidden " >
-          {blogs && blogs.map( (blog) => (
-            <div className="bg-black rounded-2xl p-4" > 
-              <h2>{blog.title}</h2>
-              <p>{blog.content}</p>
+
+        <div className="flex gap-5 flex-wrap justify-center">
+          {blogs?.map((blog) => (
+            <div
+              key={blog.id || blog._id || blog.title}
+              className="bg-black rounded-2xl p-5 w-[300px]"
+            >
+              <h2 className="text-white text-xl font-semibold">
+                {blog.title}
+              </h2>
+              <p className="text-gray-300 mt-2">
+                {blog.content}
+              </p>
             </div>
-          ) )}
+          ))}
         </div>
       </div>
     </>
+
   )
 }
 
